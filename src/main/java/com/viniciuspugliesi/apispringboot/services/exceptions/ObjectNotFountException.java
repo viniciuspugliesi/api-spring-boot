3db3b9b43dc2fc688537cc2535c5d3b202c9305b
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-09-28 14:22:49 -0300

    Exceptions and utils added

---
+++ src/main/java/com/viniciuspugliesi/apispringboot/services/exceptions/ObjectNotFountException.java
@@ -0,0 +1,13 @@
+package com.viniciuspugliesi.apispringboot.services.exceptions;
+
+public class ObjectNotFountException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFountException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFountException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
