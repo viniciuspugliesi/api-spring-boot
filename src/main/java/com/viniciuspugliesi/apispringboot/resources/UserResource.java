7916e659f27dac548d6d832387d7a5d437c048d7
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-09-28 12:05:10 -0300

    Test Domain

--- src/main/java/com/viniciuspugliesi/apispringboot/resources/UserResource.java
+++ src/main/java/com/viniciuspugliesi/apispringboot/resources/UserResource.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.viniciuspugliesi.apispringboot.domain.User;
+
 @RestController
 @RequestMapping(value="users")
 public class UserResource {
 
 	@RequestMapping(value="",method=RequestMethod.GET)
 	public String index() {
-		return "Rest funcionando.";
+		User user = new User(null, "Vinicius", "viniciuspugliesi20@gmail.com");
+		return user.toString();
 	}
 }
