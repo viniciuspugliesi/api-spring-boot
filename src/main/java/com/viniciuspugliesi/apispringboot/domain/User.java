c93dcc498ccdf2952c9af14aa4846588b5910960
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-09-28 12:24:38 -0300

    Test Entity / H2 / Persistence

--- src/main/java/com/viniciuspugliesi/apispringboot/domain/User.java
+++ src/main/java/com/viniciuspugliesi/apispringboot/domain/User.java
@@ -2,9 +2,14 @@ package com.viniciuspugliesi.apispringboot.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Integer id;
 	private String name;
 	private String email;
