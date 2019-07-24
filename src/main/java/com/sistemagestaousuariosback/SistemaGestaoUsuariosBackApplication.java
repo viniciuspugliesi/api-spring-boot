10b11a9370e74d772f8b0742c60ddfd6dab8c24b
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2019-07-24 16:30:47 -0300

    Basic Spring Boot structure project

---
+++ src/main/java/com/sistemagestaousuariosback/SistemaGestaoUsuariosBackApplication.java
@@ -0,0 +1,13 @@
+package com.sistemagestaousuariosback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoUsuariosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoUsuariosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemagestaousuariosback/SistemaGestaoUsuariosBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sistemagestaousuariosback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaGestaoUsuariosBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
